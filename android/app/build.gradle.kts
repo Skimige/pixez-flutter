922736ad6641fc80eb3b087e2b188c49b2cd7c53
Skimige <me@xyx.moe>
2025-07-28 13:01:14 +0800

    Try to fix Gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -91,7 +91,7 @@ android {
             isEnable = true
             reset()
             include("armeabi-v7a", "arm64-v8a", "x86_64")
-            isUniversalApk = true
+            isUniversalApk = false
         }
     }
 
